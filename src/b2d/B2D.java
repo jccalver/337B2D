8015c61ef7e5ff119337661028bb3b3b8ba60e1f
= <=jccalver@oakland.edu>
2016-01-21 16:51:54 -0500

    added logic processing code

--- src/b2d/B2D.java
+++ src/b2d/B2D.java
@@ -1,19 +1,30 @@
 package b2d;
-
+import java.util.Scanner;
 
 public class B2D 
 {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Welcome to the Binary to \
-		Decimal calculator Program\n");
+		System.out.println("Welcome to the Binary to Decimal calculator Program\n");
 		System.out.println("Enter binary number value: ");
 		Scanner scanner = new Scanner(System.in);
 		String buffer = scanner.nextLine();
-		System.out.println("You entered: " + buffer + ”\
-			for conversion.\n");
+		System.out.println("You entered: " + buffer + " for conversion.\n");
 		System.out.println("Proceding the data ....\n");
+                System.out.println( Binary2Decimal( buffer ));
 	}
+        
+        private static int Binary2Decimal(String binary) 
+        {
+            int result = 0;
+            for(int i = 0; i < binary.length(); i++)
+            {
+                int digit = Character.getNumericValue(binary.charAt(i));
+                int position = binary.length() - i -1;
+                result += digit * Math.pow(2, position);
+            }
+            return result;
+        }
 }
     
