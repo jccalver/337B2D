5f5716e1f8942a0af449797af3471bea534c3c44
= <=jccalver@oakland.edu>
2016-01-21 16:15:36 -0500

    entered program code

--- src/b2d/B2D.java
+++ src/b2d/B2D.java
@@ -1,21 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package b2d;
 
-/**
- *
- * @author Jonca_000
- */
-public class B2D {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+public class B2D 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to the Binary to \
+		Decimal calculator Program\n");
+		System.out.println("Enter binary number value: ");
+		Scanner scanner = new Scanner(System.in);
+		String buffer = scanner.nextLine();
+		System.out.println("You entered: " + buffer + ”\
+			for conversion.\n");
+		System.out.println("Proceding the data ....\n");
+	}
 }
+    
